2cd18c887de6fd819f843de32ee901ef969f783b
Vincent <vmgboyles@gmail.com>
2026-08-16 23:09:02 +0800

    redid functions to create and add book to library

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 const myLibrary = [];
 
-function Book() {
+function Book( title, author, pages, read) {
   // the constructor...
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
+  const newBook = new Book(title, author, pages, read)
+  myLibrary.push(newBook)
 }
+
+function displayBook() {
+  for (book in myLibrary) {
+    
+  }
+}
+
+
+addBookToLibrary("Man's Search for Meaning", "Viktor Frankl", 450, true)
+addBookToLibrary("Atlas Shrugged", "Ayn Rand", 300, false)
+console.log(myLibrary[0])
+
+
