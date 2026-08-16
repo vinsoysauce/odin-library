bc1c87d385780cefe7162ed59d31693d5e4b9f4b
Vincent <vmgboyles@gmail.com>
2026-08-17 03:40:20 +0800

    created basic logic and function for displaybook

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book( title, author, pages, read) {
+function Book( title, author, read, pages) {
   // the constructor...
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
@@ -8,25 +8,57 @@ function Book( title, author, pages, read) {
   this.id = crypto.randomUUID();
   this.title = title;
   this.author = author;
-  this.pages = pages;
   this.read = read;
+  this.pages = pages;
 }
 
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary(title, author, read, pages) {
   // take params, create a book then store it in the array
-  const newBook = new Book(title, author, pages, read)
+  const newBook = new Book(title, author, read, pages)
   myLibrary.push(newBook)
 }
 
 function displayBook() {
-  for (book in myLibrary) {
-    
+  const container = document.querySelector(".books-container");
+  for (item in myLibrary) {
+  const book = document.createElement("div");
+  book.classList.add("book");
+    for (value in myLibrary[item]) {
+      if (myLibrary[item][value] === myLibrary[item].title) {
+        const title = document.createElement("div");
+        title.classList.add("title")
+        title.textContent += (myLibrary[item][value]);
+        book.appendChild(title)
+      } else if (myLibrary[item][value] === myLibrary[item].author) {
+        const author = document.createElement("div");
+        author.classList.add("author")
+        author.textContent += (myLibrary[item][value]);
+        book.appendChild(author)
+      } else if (myLibrary[item][value] === myLibrary[item].read) {
+        const read = document.createElement("div");
+        read.classList.add("read");
+        if (myLibrary[item].read) {
+          read.textContent += 'Status: Read';
+        } else {
+          read.textContent += 'Status: Not read yet';
+        }
+        book.appendChild(read);
+      } else if (myLibrary[item][value] === myLibrary[item].pages) {
+          const pages = document.createElement("div");
+          pages.classList.add("pages")
+          pages.textContent += (myLibrary[item][value]);
+        book.appendChild(pages)
+      }
+    }
+    container.appendChild(book)
   }
 }
 
 
-addBookToLibrary("Man's Search for Meaning", "Viktor Frankl", 450, true)
-addBookToLibrary("Atlas Shrugged", "Ayn Rand", 300, false)
-console.log(myLibrary[0])
+
+
+addBookToLibrary("Man's Search for Meaning", "Viktor Frankl", true, 250)
+addBookToLibrary("Atlas Shrugged", "Ayn Rand", false, 300)
+displayBook()
 
 
