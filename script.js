806e121ab1fa2da63685da21fda976e4e9fc4929
Vincent <vmgboyles@gmail.com>
2026-08-21 18:02:37 +0800

    adding books through add book button works wwithout duplication

--- script.js
+++ script.js
@@ -23,11 +23,67 @@ function addBookToLibrary(title, author, read, pages) {
   // take params, create a book then store it in the array
   const newBook = new Book(title, author, read, pages)
   myLibrary.push(newBook)
+  displayBook()
 }
+
+// function displayBook() {
+//   for (item in myLibrary[myLibrary.length - 1]) {
+//   const container = document.querySelector(".books-container");
+//   const bookCard = document.createElement("div");
+//   bookCard.classList.add("bookCard");
+//   const book = document.createElement("div");
+//   book.classList.add("book");
+//   const bookPages = document.createElement("div");
+//   bookPages.classList.add("bookPages");
+//   const details  = document.createElement("div");
+//   details.classList.add("book-details");
+//   const actions = document.createElement("div");
+//   actions.classList.add("book-actions");
+//     for (value in myLibrary[item]) {
+//       if (myLibrary[item][value] === myLibrary[item].title) {
+//         const title = document.createElement("div");
+//         title.classList.add("title")
+//         title.textContent = (myLibrary[item][value]);
+//         details.appendChild(title)
+//         book.appendChild(details)
+//       } else if (myLibrary[item][value] === myLibrary[item].author) {
+//         const author = document.createElement("div");
+//         author.classList.add("author")
+//         author.textContent = (myLibrary[item][value]);
+//         details.appendChild(author)
+//         book.appendChild(details)
+//       } else if (myLibrary[item][value] === myLibrary[item].read) {
+//         const read = document.createElement("div");
+//         read.classList.add("read");
+//         if (myLibrary[item].read) {
+//           read.textContent = 'Status: Read';
+//         } else {
+//           read.textContent = 'Status: Not read yet';
+//         }
+//         details.appendChild(read)
+//         book.appendChild(details)
+//       } else if (myLibrary[item][value] === myLibrary[item].pages) {
+//         const removeBook = document.createElement("img");
+//         removeBook.classList.add("remove-book");
+//         removeBook.src = "/svg/remove.svg";
+//         const readOrUnread = document.createElement("img");
+//         readOrUnread.classList.add("readOrUnread");
+//         readOrUnread.src = "/svg/read.svg";
+//         actions.appendChild(removeBook)
+//         book.appendChild(actions)
+//         actions.appendChild(readOrUnread);
+//         book.appendChild(actions)
+//         bookPages.textContent = (myLibrary[item][value] + " pages");
+//       }
+//     }
+//     bookCard.appendChild(book);
+//     bookCard.appendChild(bookPages);
+//     container.appendChild(bookCard);
+//   }
+// }
 
 function displayBook() {
   const container = document.querySelector(".books-container");
-  for (item in myLibrary) {
   const bookCard = document.createElement("div");
   bookCard.classList.add("bookCard");
   const book = document.createElement("div");
@@ -38,30 +94,31 @@ function displayBook() {
   details.classList.add("book-details");
   const actions = document.createElement("div");
   actions.classList.add("book-actions");
-    for (value in myLibrary[item]) {
-      if (myLibrary[item][value] === myLibrary[item].title) {
+  const lastBook = myLibrary[myLibrary.length - 1]
+  for (const item in lastBook) {
+      if (lastBook[item] === lastBook.title) {
         const title = document.createElement("div");
         title.classList.add("title")
-        title.textContent += (myLibrary[item][value]);
+        title.textContent = (lastBook[item]);
         details.appendChild(title)
         book.appendChild(details)
-      } else if (myLibrary[item][value] === myLibrary[item].author) {
+      } else if (lastBook[item] === lastBook.author) {
         const author = document.createElement("div");
         author.classList.add("author")
-        author.textContent += (myLibrary[item][value]);
+        author.textContent = (lastBook[item]);
         details.appendChild(author)
         book.appendChild(details)
-      } else if (myLibrary[item][value] === myLibrary[item].read) {
+      } else if (lastBook[item] === lastBook.read) {
         const read = document.createElement("div");
         read.classList.add("read");
-        if (myLibrary[item].read) {
-          read.textContent += 'Status: Read';
+        if (lastBook.read) {
+          read.textContent = 'Status: Read';
         } else {
-          read.textContent += 'Status: Not read yet';
+          read.textContent = 'Status: Not read yet';
         }
         details.appendChild(read)
         book.appendChild(details)
-      } else if (myLibrary[item][value] === myLibrary[item].pages) {
+      } else if (lastBook[item] === lastBook.pages) {
         const removeBook = document.createElement("img");
         removeBook.classList.add("remove-book");
         removeBook.src = "/svg/remove.svg";
@@ -72,18 +129,19 @@ function displayBook() {
         book.appendChild(actions)
         actions.appendChild(readOrUnread);
         book.appendChild(actions)
-        bookPages.textContent += (myLibrary[item][value] + " pages");
+        bookPages.textContent = (lastBook[item] + " pages");
       }
     }
     bookCard.appendChild(book);
     bookCard.appendChild(bookPages);
     container.appendChild(bookCard);
   }
-}
+
+
 
 
 const openBtn = document.getElementById("add-book-button");
-const closeBtn = document.getElementById("cancel-book-button");
+const closeBtn = document.getElementById("cancel");
 const modal = document.getElementById("modal");
 
 openBtn.addEventListener("click", () => {
@@ -94,19 +152,29 @@ closeBtn.addEventListener("click", () => {
   modal.classList.remove("open");
 })
 
-const addBookButton = document.querySelector('#add-book-button')
-addBookButton.onclick = () => {
-  
-}
 
+// const addBookButton = document.querySelector('#add-book-button')
+//   addBookButton.onclick = () => {
+//     console.log(form.elements[0])
+//   addBookToLibrary(form.elements[0], form.elements[1], form.elements[2], form.elements[3], form.elements[4])
+//   event.preventDefault();
+// }
 
+const form = document.getElementById('form')
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const title = document.getElementById('book_title').value;
+    const author = document.getElementById('book_author').value;
+    const read = true;
+    const pages = Number(document.getElementById('book_pages').value)
+    addBookToLibrary(title, author, read, pages);
+    modal.classList.remove("open");
+    form.reset();
+});
 
 addBookToLibrary("Man's Search for Meaning", "Viktor Frankl", false, 184)
 addBookToLibrary("Tuesdays with Morrie", "Mitch Albom", false, 224)
 addBookToLibrary("The Three Body Problem", "Cixin Liu", false, 416)
 addBookToLibrary("The Dark Forest", "Cixin Liu", false, 528)
 addBookToLibrary("Death's End", "Cixin Liu", false, 624)
-displayBook()
-
-
 
